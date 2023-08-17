92825440f59f8fd0f546f5fd184e8ded67797bee
hien nguyen <thuhiennguyen198@gmail.com>
2023-08-16 22:18:42 -0400

    remove services

--- user-service/src/main/java/com/b4u/user/constant/ApplicationConstant.java
+++
@@ -1,6 +0,0 @@
-package com.b4u.user.constant;
-
-public class ApplicationConstant {
-    public static final int STATUS_FAIL = 201;
-    public static final int STATUS_SUCCESS = 200;
-}
--- user-service/src/main/java/com/b4u/user/domain/RoleName.java
+++
@@ -1,7 +0,0 @@
-package com.b4u.user.domain;
-
-public enum RoleName {
-    ROLE_ADMIN,
-    ROLE_USER,
-    ROLE_PRODUCT_ADMIN
-}
